a9dc1ec6457695de6d6afd1bbe91aae6cf5cf284
Jacob <151932726+j-ble@users.noreply.github.com>
2025-10-20 16:36:55 -0700

    Time: 3 ms (62.15%) | Memory: 18.8 MB (34.44%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -1,12 +1,10 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        # create a hashset to store nums
-        hashset = set(nums)
-        # range of numbers
+        # space complexity O(1) & time complexity O(n)
+        # find the length of the array
         n = len(nums)
-        # check every number within the range 0 + n
-        for i in range(n + 1):
-            # if the number is not seen in the set
-            if i not in hashset:
-                # return it
-                return i
+        # find the range/length of nums
+        for i in range(len(nums)):
+            # add number and subtract actual number found
+            n += (i - nums[i])
+        return n
