01df975dd061bfc13ecf5f4e49068d36edd33d9d
Jacob <151932726+j-ble@users.noreply.github.com>
2025-10-21 07:40:25 -0700

    Time: 58 ms (8.30%) | Memory: 30.7 MB (81.37%) - LeetSync

--- 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.py
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.py
@@ -1,15 +1,20 @@
 class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
-        # Create a hashset to store nums
-        hashset = set(nums)
-        # Create an array to store missing nums
+        # O(n) time complexity with O(1) space complexity
+        # The problem states that every value in the array corresponds to a valid index
+        # mark the existing nums (go through every value within nums)
+        for n in nums:
+            # Create a mapping of n to some index
+            i = abs(n) - 1
+            # update the nums array to negative values (shows which values exist in the array and do not exist)
+            nums[i] = -1 * abs(nums[i])
+        # build the result
         res = []
-        # define the length of nums
-        n = len(nums)
-        # Find the range of nums
-        for i in range(1, n + 1):
-            # if i is not in the hashset, append missing num to results array
-            if i not in hashset:
-                res.append(i)
+        # use enumerate to track the index and the number
+        for i, n in enumerate(nums):
+            # if the number is positive then it does not exist within the input array
+            if n > 0:
+                # we solved this using our index + 1. So we want to append our result to the index + 1
+                res.append(i + 1)
         # return the result
         return res
