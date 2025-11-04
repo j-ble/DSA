4aa89aa3748c517828c05d40beda36d7a0a091bd
Jacob <151932726+j-ble@users.noreply.github.com>
2025-11-04 12:15:12 -0700

    Time: 12 ms (66.13%) | Memory: 22.9 MB (78.95%) - LeetSync

--- 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.py
+++ 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.py
@@ -1,22 +1,15 @@
 class Solution:
     def construct2DArray(self, original: List[int], m: int, n: int) -> List[List[int]]:
-        # 0-indexed 1-dimensional (1D) integer array original
-        # e.g., [1,2,3,4]
-        # two integers, m and n
-        # using all the elements from original
-        # if the length of original is odd return array (e.g., example 2)
+        # edge case: if n * m != orginal return the array
         if len(original) != n * m:
             return []
-        # create a result array to store new 2D Array
+        # create an empty result array to store result
         res = []
-        # conduct the new 2D array
-        # new range for the 2D array (why? tells which slice of the original array to grab)
-        # we need to loop m times becuase we need m rows
+        # each row is created by the range(m)
         for r in range(m):
-            # get our starting index (current row * n)
+            # start index (which is the row * n)
             start = r * n
-            # get our ending index
+            # end index (which is the start + n)
             end = start + n
-            # slice the original 
             res.append(original[start:end])
         return res
