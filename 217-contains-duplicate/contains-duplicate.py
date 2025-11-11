72bbd19b7efe5d16277818adfb2bfcd3d690181f
Jacob <151932726+j-ble@users.noreply.github.com>
2025-11-11 12:05:09 -0700

    Time: 10 ms (78.97%) | Memory: 31.5 MB (53.81%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,16 +1,8 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        # optimize for space complexity rather than time complexity
-        # sort each nums
-        nums.sort()
-        # iterate thoruhg sorted array
-        for i in range(len(nums) - 1):
-            # Compare each element with the one after it
-            if nums[i] == nums[i+1]:
-                # If the same return true
-                return True
-        # otherwise return False
+        # Space Complexity is O(n) since we create a hashSet
+        # time complexity is O(n) since we iterate through array
+        hashSet = set(nums)
+        if len(nums) != len(hashSet):          
+            return True
         return False
-
-
-        
